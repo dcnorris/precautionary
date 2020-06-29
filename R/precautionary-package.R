fa5b3abae163fd006b984fb8b34e3d3aa7d6968e
David Norris <david@UnusualSolutionsThatWork.com>
2020-06-29 06:21:45 -0400

    package:precautionary skeleton/sketch init commit

---
+++ R/precautionary-package.R
@@ -0,0 +1,50 @@
+#' @name precautionary-package
+#' @title Precautionary diagnostics for dose-escalation trial designs
+#' @description Enhances various R packages that support the design and simulation
+#' of phase 1 dose-escalation trials, adding diagnostics to examine the safety
+#' characteristics of these designs in light of expected inter-individual variation
+#' in pharmacokinetics and pharmacodynamics.
+#' @aliases precautionary-package precautionary
+#' @docType package
+#' @author David C. Norris
+#' 
+#' @references
+#' 1. Norris DC. Dose Titration Algorithm Tuning (DTAT) should supersede
+#'    \sQuote{the} Maximum Tolerated Dose (MTD) in oncology dose-finding trials.
+#'    \emph{F1000Research}. 2017;6:112. \doi{10.12688/f1000research.10624.3}.
+#'    \url{https://f1000research.com/articles/6-112/v3}
+#' 
+#' 2. Norris DC. Costing \sQuote{the} MTD. \emph{bioRxiv}. August 2017:150821.
+#'    \doi{10.1101/150821}.
+#'    \url{http://www.biorxiv.org/content/early/2017/08/22/150821}
+#' 
+#' 3. Norris DC. Precautionary Coherence Unravels Dose Escalation Designs.
+#'    \emph{bioRxiv}. December 2017:240846. \doi{10.1101/240846}.
+#'    \url{https://www.biorxiv.org/content/early/2017/12/29/240846}
+#' 
+#' 4. Norris DC. One-size-fits-all dosing in oncology wastes money, innovation
+#'    and lives. \emph{Drug Discov Today}. 2018;23(1):4-6.
+#'    \doi{10.1016/j.drudis.2017.11.008}.
+#'    \url{https://www.sciencedirect.com/science/article/pii/S1359644617303586}
+#' 
+#' 5. Norris DC. Costing \sQuote{the} MTD ... in 2-D. \emph{bioRxiv}. July 2018:370817.
+#'    \doi{10.1101/370817}.
+#'    \url{https://www.biorxiv.org/content/early/2018/07/17/370817}
+#' 
+#' 6. Norris DC. Ethical Review and Methodologic Innovation in Phase 1 Cancer Trials.
+#'    \emph{JAMA Pediatrics}. 2019;173(6):609
+#'    \doi{10.1001/jamapediatrics.2019.0811}.
+#'    
+#' 7. Norris DC. Comment on Wages et al., Coherence principles in interval-based
+#'    dose finding. Pharmaceutical Statistics 2019, DOI: 10.1002/pst.1974.
+#'    \emph{Pharmaceutical Statistics}. March 2020.
+#'    \doi{10.1002/pst.2016}.
+#'    
+#' 8. Norris DC. Retrospective analysis of a fatal dose-finding trial.
+#'    arXiv:2004.12755 [stat, q-bio]. April 2020.
+#'    \url{http://arxiv.org/abs/2004.12755}
+#' 
+#' @importFrom distr6 Lognormal Gamma
+#' @import methods
+NULL
+
