7687ffee51bf3bd9afdf6a7c759dc21f760dbb89
David Norris <david@UnusualSolutionsThatWork.com>
2020-07-02 07:43:49 -0400

    Fold sim_safety into simulate_trials("ordtox")
    Used callNextMethod to avoid infinite loop

--- R/simulate_trials.R
+++ R/simulate_trials.R
@@ -18,8 +18,102 @@ setOldClass("three_plus_three_selector_factory")
 #' @rdname simulate_trials
 setMethod("simulate_trials", c(selector_factory="ordtox"),
   function(selector_factory, num_sims, true_prob_tox, ...){
-    sim_safety(selector_factory, ...)
-  })
+    protocol <- selector_factory # separate naming from implementation details
+    lambda_CV = 3
+    median_mtd = protocol$num_doses - 1
+    median_sd = median_mtd/3
+    r0 = seq(0.5, 2.5, 0.5)
+    K = num_sims
+    M = num_sims
+    # TODO: Consider making true_prob_tox argument optionally
+    #       a data table (or data frame) of the following form,
+    #       _OR_ (more usefully) a *generative model* for same.
+    true_prob_tox <- data.table(CV = rexp(n=K, rate=lambda_CV))
+    true_prob_tox[, `:=`(
+      sigma = sqrt(log(CV^2 + 1)) # NB: CV>sigma, w/ near-identity below 0.4
+      , mu = rnorm(n=K, mean=median_mtd, sd=median_sd)
+    )]
+    for(d in seq(protocol$num_doses)){
+      true_prob_tox[, paste0("P",d) := pnorm(q=d, mean=mu, sd=sigma)]
+    }
+    setcolorder(true_prob_tox, paste0("P", 1:protocol$num_doses)) # move P1..Pd to front
+    ensembles <<- list()
+    toxicities <<- list()
+    for(k in 1:K){
+      cat("k =", k, "\n")
+      # Now invoke the default method from package 'escalation':
+      sims <- callNextMethod(protocol, num_sims,
+                             true_prob_tox = as.numeric(true_prob_tox[k, 1:protocol$num_doses]))
+      ensemble <- rbindlist(lapply(sims[[1]], function(.) .[[1]]$fit$outcomes)
+                            , idcol = "rep")
+      ensemble <- merge(data.frame(r0=r0), ensemble) # cartesian product
+      ensemble <- as.data.table(ensemble) # restore data.table after cartesian product
+      ensemble[, MTDi3 := qnorm(p = u_i
+                                , mean = true_prob_tox[k]$mu
+                                , sd = true_prob_tox[k]$sigma
+      )]
+      ensemble[, `:=`(
+        MTDi1 = MTDi3 - 2*r0
+        , MTDi2 = MTDi3 - r0
+        , MTDi4 = MTDi3 + r0
+        , MTDi5 = MTDi3 + 2*r0
+      )]
+      ensemble[, toxgrade := (dose>MTDi1) + (dose>MTDi2) + (dose>MTDi3) +
+                 (dose>MTDi4) + (dose>MTDi5)]
+      stopifnot(with(ensemble, all(tox == as.integer(dose >= MTDi3))))
+      # TODO: The following summary is wrong. I need to write the ensemble
+      #       to .GlobalEnv, and experiment 'by hand' to find the right
+      #       summary scheme.
+      # N.B.: Any trial realization ('rep') is just as likely as any other.
+      #       Therefore, I have to aggregate with reps as the denominator.
+      #       Specifically, pooling all the patients would fail to weight
+      #       them inversely to trial size.
+      #       Another way to see this is, we are asking about the probability
+      #       of a fatality in *this* trial. Alternatively, we can ask for
+      #       the expected number of each grade of toxicity in *this* trial.
+      #       Note that this should partition the expected size of this trial!
+      ensemble[, Tox := factor(toxgrade, levels=0:5, labels=paste0("Gr",0:5))]
+      ensembles[[k]] <<- ensemble
+      # See https://stackoverflow.com/a/16519612/3338147 explaining below syntax
+      toxicities[[k]] <<- ensemble[, .(Tox=ordered(levels(Tox)), N=c(table(Tox)))
+                                   , by=r0]
+      # Importantly, because these protocols run considering only DLT = Gr>=3,
+      # they are r0-agnostic. But I ought to allow for the general case where
+      # ordinal toxicities (MTDig for g != 3) affect trial conduct.
+      #
+      # Initially, let me go simply for *counts* and worry secondarily about
+      # obtaining (and dividing by) the denominators.
+      # Want a tabulation with several r0 values defining rows, and columns for
+      # the counts of toxicity grades. A final column may show total enrollments,
+      # which as noted above will all be equal unless ordinal toxicities affect
+      # escalation or termination decisions.
+    }
+    # Indeed, I like the idea of abstracting the calculation of high-level
+    # summary statistics into a separate function, possibly even a 'summary'
+    # method for a suitably defined R3 class. But for now, let me implement
+    # these summaries here, as additional components of the returned list.
+    toxdt <- rbindlist(toxicities, idcol = "k")
+    counts <- toxdt[, .(n=sum(N)), by=.(r0,Tox)]
+    toxtab <- ftable(xtabs(n ~ Tox + r0, counts))
+    # It now seems to me there are 2 perspectives on the probabilities here.
+    # One POV is the per-trial perspective, which might ask for expected
+    # numbers of each grade of toxicity.
+    # The second POV is that of the enrolling patient, who might ask what
+    # is the probability of each grade of toxicity. But this POV ought to
+    # be conditioned on the prior results seen in the trial, and so demands
+    # much more sophisticated modeling -- indeed, modeling of the kind I
+    # employed in the AFM11 paper.
+    # Note in fact that, as soon as such a perspective BEGINS to be acknowledged,
+    # I have already 'won' the argument about dose individualization!
+    list(tpt = true_prob_tox
+        ,toxdt = toxdt
+        ,toxtab = toxtab
+        ,expect = toxtab/(K*M)
+        ,K = K
+        ,M = M
+        )
+  }
+)
 
 #' Syntactic sugar for injecting safety analysis into \code{simulate_trials}
 #'
