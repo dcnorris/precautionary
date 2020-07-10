93a78785e3474402131e656918e54addd450847f
David Norris <david@UnusualSolutionsThatWork.com>
2020-07-10 06:28:46 -0400

    Expunge 'tox_probs_at' generic & methods

--- R/toxicity_generators.R
+++ R/toxicity_generators.R
@@ -101,62 +101,6 @@ setMethod("plot", "mtdi_distribution", function(x, y=NULL, ...) {
   }
 })
 
-# This function may offer excellent information-hiding access
-# to the optonal @ordinalizer of its first argument.
-# The return value may simply have an optional 'ordtox' attribute
-# that mirrors the optionality of the @ordinalizer. After invoking
-# 'tox_probs_at', client code can check whether this attribute
-# exists, and act accordingly.
-# The ... argument provides an obvious means to pass further
-# information like r0 (or r[1,2,4,5]) to the @ordinalizer.
-
-#' Obtain toxicity probabilities from an mtdi_generator
-#'
-#' @param mtdi An \code{mtdi_generator}
-#' @param doses A vector of (dimensioned) doses at which to calculate
-#' the toxicity probabilities.
-#' @param ... Arguments passed to \code{mtdi}'s ordinalizer
-#'
-#' @return A suitable data structure (e.g., vector or matrix) of
-#' binary-toxicity probabilities, with an optional \sQuote{ordtox}
-#' attribute (in case \code{mtdi} has a non-NULL ordinalizer slot)
-#' that spreads this into the further dimension of multiple ordinal
-#' toxicities.
-#' @export
-setGeneric("tox_probs_at", function(mtdi, doses, ...) {
-  standardGeneric("tox_probs_at")
-})
-
-#' @examples 
-#' mtdi_gen <- mtdi_lognormal(CV = 0.5
-#'                           ,median = 140
-#'                           ,units = "ng/kg/week")
-#' doses <- seq(100, 150, 10)
-#' mtdi_gen %>% tox_probs_at(doses)
-#' # Now attach a proper ordinalizer to 'mtdi_gen':
-#' mtdi_gen@ordinalizer <- function(dose, r0) {
-#'   c(Gr1=dose*r0^2, Gr2=dose*r0, Gr3=dose, Gr4=dose/r0, Gr5=dose/r0^2)
-#' }
-#' tpa <- mtdi_gen %>% tox_probs_at(doses, r0=1.5)
-#' tpa
-#' @rdname tox_probs_at
-#' @export
-setMethod(
-  "tox_probs_at"
-  , c("mtdi_distribution", "numeric"),
-  function(mtdi, doses, ...){
-    probs <- mtdi@dist$cdf(doses)
-    names(probs) <- paste0(doses, mtdi@units)
-    if( !is.null(body(mtdi@ordinalizer)) ) {
-      doses_matrix <- sapply(doses, mtdi@ordinalizer, ...)
-      # Trick below comes via https://stackoverflow.com/a/8579498/3338147:
-      probs_matrix <- doses_matrix # template structure
-      probs_matrix[] <- vapply(doses_matrix, mtdi@dist$cdf, numeric(1))
-      colnames(probs_matrix) <- paste0(doses, mtdi@units)
-      attr(probs,'ordtox') <- probs_matrix
-    }
-    probs
-  })
 
 #' @export mtdi_lognormal
 mtdi_lognormal <- setClass("mtdi_lognormal",
@@ -213,48 +157,5 @@ setMethod(
           , MoreArgs = list(units = hyper@units
                            ,ordinalizer = hyper@ordinalizer)
           , SIMPLIFY = FALSE)
-  })
-
-#' @examples 
-#' mtdi_gen <- hyper_mtdi_lognormal(lambda_CV = 3
-#'                                  , median_mtd = 6, median_sd = 2
-#'                                  , units="mg/kg")
-#' doses <- c(0.5, 1, 2, 4, 6, 8)
-#' mtdi_gen %>% tox_probs_at(doses, K=3)
-#' # Now attach a proper ordinalizer to 'mtdi_gen':
-#' mtdi_gen@ordinalizer <- function(dose, r0) {
-#'   c(Gr1=dose*r0^2, Gr2=dose*r0, Gr3=dose, Gr4=dose/r0, Gr5=dose/r0^2)
-#' }
-#' tpa <- mtdi_gen %>% tox_probs_at(doses, K=3, r0=1.5)
-#' tpa
-#' ordtox <- attr(tpa,'ordtox')
-#' class(ordtox)
-#' dimnames(ordtox)
-#' @rdname tox_probs_at
-#' @export
-setMethod(
-  "tox_probs_at"
-  , c("hyper_mtdi_lognormal","numeric"),
-  function(mtdi, doses, K, ...) {
-    # Generate a list of K mtdi_lognormal objects:
-    mtdi_list <- draw_samples(hyper = mtdi, K = K)
-    ## TEST whether the elements of mtdi_list have 'ordtox' attribute
-    cat("attr(mtdi_list[[1]],'ordtox') = ")
-    print(attr(mtdi_list[[1]],'ordtox'))
-    # Transform this list to a list of tox_probs_at results:
-    tpa_list <- lapply(mtdi_list, tox_probs_at, doses, ...)
-    # Generate a higher-dimensional version of the result
-    # yielded by tox_probs_at("mtdi_distribution") method.
-    # This will be a K-row matrix of sample tox probabilities,
-    # with an optional 'ordtox' attribute that is an *array*.
-    result <- do.call(rbind, tpa_list)
-    ordtox <- sapply(tpa_list, attr, which='ordtox', simplify="array")
-    if(all(sapply(ordtox, is.null))) ordtox <- NULL
-    # TODO: Consider naming the 3rd dimension or ordtox
-    #       with labels like 'sample1', 'sample2', ...
-    #       or perhaps "k=1", "k=2", ... in case I wish
-    #       to settle on 'k' as a sample index.
-    attr(result,'ordtox') <- ordtox
-    result
   })
 
