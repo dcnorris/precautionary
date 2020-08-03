fbd84712e783f6e44032ad393c85e1fd3b534f35
David Norris <david@UnusualSolutionsThatWork.com>
2020-08-03 07:31:19 -0400

    Add a more concise alternative

--- exec/prolog/three3.pl
+++ exec/prolog/three3.pl
@@ -2,10 +2,10 @@
 
 
 % Predefined dose levels
-dose(D) :- member(D, [1,2,3,4,5]).
+dose(D) :- between(1, 5, D).
 
 % 0 - 3 DLTs per cohort
-tox(N) :- member(N, [0,1,2,3]).
+tox(N) :- between(0, 3, N).
 
 % TODO: Distinguish an MTD-not-found terminal case.
 
@@ -31,3 +31,40 @@ escalate(D1, [D ^ 0 | Prev]) :-
     ;   stay(D, Prev)
     ).
 
+
+% ---- Attempt a more concise program ----
+
+declare(D, [D ^ 1, D ^ 1 | E]) :-
+    escalation(D, E).
+declare(D_1, [D ^ T | E]) :-
+    escalation(D, E),
+    tox(T), T > 1,
+    D_1 is D - 1.
+
+escalation(1, []).
+
+escalation(D1, [D ^ 0 | E]) :-
+    dose(D1),
+    D is D1 - 1,
+    D > 0,
+    escalation(D, E).
+
+% In these goals, should I be unifying E with
+% a more detailed representation?
+
+% Do I need 2 different predicates, next/2 and trial/2?
+next(D, E) :-
+    (	up(D, E)
+    ;	sta(D, E)
+    ).
+up(1, []).
+up(U, [D^0 | E]) :- dose(D), U is D+1, next(D,E).
+sta(D, [D^1 | E]) :- up(D, E).
+mtd(D, [D^1 | E]) :- sta(D, E).
+mtd(D_, [D^T | E]) :- tox(T), T>1, up(D, E), D_ is D-1.
+result(R, E) :- % TODO: Make #doses a parameter
+    (	mtd(D, E), R = mtd(D)
+    ;	up(6, E), R = mtd_not_found
+    ).
+
+% TODO: Try a formulation via PRODUCTION RULES!
