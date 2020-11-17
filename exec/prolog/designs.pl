4e2544e2f95f9c3f3d46fbae473fbbe99072a41d
David Norris <david@UnusualSolutionsThatWork.com>
2020-11-17 17:02:49 -0500

    Top-to-bottom review w/ important repairs

--- exec/prolog/designs.pl
+++ exec/prolog/designs.pl
@@ -1,13 +1,10 @@
 % Attempt an enumeration of ALL dose-finding designs over 2 doses
 
-/* DISCUSS: How to switch on Prolog system?
+/* I have moved these to implementation-specific init files
+:- use_module(library(clpfd)).  % SWI
+:- use_module(library(clpz)).   % Scryer
 */
 
-:- use_module(library(clpfd))  % SWI
-   ;use_module(library(clpz)). % Scryer
-
-%:- use_module(library(clpfd)).  % SWI
-%:- use_module(library(clpz)). % Scryer
 :- use_module(library(pio)).
 :- use_module(library(pairs)).
 
@@ -19,8 +16,6 @@
 denominator(N1 - N2) :-
     N1 in 0 \/ 3 \/ 6,
     N2 in 0 \/ 3 \/ 6.
-%    member(N1, [0, 3, 6]), % TODO: N1 in 0 \/ 3 \/ 6
-%    member(N2, [0, 3, 6]).
 
 %?- denominator(X - Y).
 %@ X in 0\/3\/6,
@@ -50,9 +45,7 @@ denominator(N1 - N2) :-
 %@ X in 2..3.
 
 %?- tuples_in([[N1]], [[0],[3],[6]]).
-%@    clpz:(N1 in 0\/3\/6)
-%@ ;  false.
-%@ N1 in 0\/3\/6. # SWI
+%@ N1 in 0\/3\/6.
 
 %?- denominator(X - Y).
 %@ X in 0\/3\/6,
@@ -64,18 +57,16 @@ numerator_denominator(T1 - T2, N1 - N2) :-
     denominator(N1 - N2),
     T1 #>= 0, T1 #=< N1,
     T2 #>= 0, T2 #=< N2.
-%    T1 in 0..N1,
-%    T2 in 0..N2.
 
 %?- numerator_denominator(T, N).
-%@ T = _51576-_51578,
-%@ N = _51594-_51596,
-%@ _51576 in 0..6,
-%@ _51594#>=_51576,
-%@ _51594 in 0\/3\/6,
-%@ _51578 in 0..6,
-%@ _51596#>=_51578,
-%@ _51596 in 0\/3\/6.
+%@ T = _39444-_39446,
+%@ N = _39462-_39464,
+%@ _39444 in 0..6,
+%@ _39462#>=_39444,
+%@ _39462 in 0\/3\/6,
+%@ _39446 in 0..6,
+%@ _39464#>=_39446,
+%@ _39464 in 0\/3\/6.
 
 /* - - -
 Note that numerator_denominator/2 enumerates all possible toxicity tallies
@@ -109,10 +100,10 @@ denominators.
 
 Incidentally, it is worth noting that rotating the above lattice
 clockwise by 135 degrees yields a TREE in which leftward movement
-corresponds to choosing the lower dose and rightward branching to
-choosing the higher dose. (Also, observe that these are not the
-same as 'escalation' or 'de-escalation' because the latter concepts
-are PATH-DEPENDENT.)
+corresponds to choosing the lower dose for the next cohort, and
+rightward branching to choosing the higher dose for the next cohort.
+(Also, observe that these are DISTINCT FROM 'escalation' and
+'de-escalation' because the latter concepts are PATH-DEPENDENT.)
 
 */
 
@@ -140,22 +131,34 @@ step_too_bold(T/6, 3/3) :- T #> 1.
 
 %?- unacceptable_tally(UT).
 %@ UT = 0/6-0/0 ;
-%@ UT = _85346/3-3/3,
-%@ _85346 in 1..sup ;
-%@ UT = _86692/6-3/3,
-%@ _86692 in 2..sup.
+%@ UT = _42196/3-3/3,
+%@ _42196 in 1..sup ;
+%@ UT = _43542/6-3/3,
+%@ _43542 in 2..sup.
 
 /*
 From the domain-expert perspective, it is far more
 natural to describe tallies that are UNacceptable.
 
-TODO: Exploit features such as #\ and tuple_in to
-      achieve such an expression.
+Accordingly, we use #\ in the following.
+
+TODO: Remain alert for opportunities to use tuples_in/2,
+      especially with the generalization to D > 2.
+
+TODO: Consider whether describing a partial order
+      on the tallies (worse-than) would help with
+      the formulation of 'acceptable'.
 */
 
+%% TODO: Consider 
+
 acceptable_tally(T1 - T2, N1 - N2) :-
-    T2 #< 5, % Want never to see 5+ toxicities at higher dose,
-    T1 #< 4, % and never even 4+ toxicities at a lower dose.
+    numerator_denominator(T1 - T2, N1 - N2),
+    % TODO: Articulate a rationale for these 2 'never' demands:
+    #\ T2 in 5..sup, % Want never to see 5+ toxicities
+    #\ T1 in 5..sup, % at either dose.
+    % Now, depending on net enrollment (so far) at the lower dose,
+    % we have some further things to say about what is acceptable...
     zcompare(C, N1, 6),
     acceptable_tally_(C, T1/N1 - T2/N2).
 
@@ -174,42 +177,76 @@ acceptable_tally_(<, T1/N1 - T2/N2) :-
 %% NB: Clause for the '>' case is purposely absent:
 %%acceptable_tally(>, _) :- false.
 
-%?- acceptable_tally(T, N).
-%@ T = _89184-_89186,
-%@ N = 6-_89204,
-%@ _89184 in 1..3,
-%@ _89186 in inf..4 ;
-%@ T = _91188-_91190,
-%@ N = 6-_91208,
-%@ _91188 in inf..3,
-%@ _91190 in inf..4,
-%@ _91208 in 1..sup ;
-%@ T = _93552-_93554,
-%@ N = _93570-0,
-%@ _93552 in inf..3,
-%@ _93554 in inf..4,
-%@ _93570 in inf..5 ;
-%@ T = 0-_95756,
-%@ N = _95772-_95774,
-%@ _95756 in inf..4,
-%@ _95772 in 3..5.
-%% TODO: Examine the above for correctness.
+%?- acceptable_tally(T1 - T2, N1 - N2).
+%@ N1 = 6,
+%@ T1 in 1..4,
+%@ T2 in 0..4,
+%@ N2#>=T2,
+%@ N2 in 0\/3\/6 ;
+% Acceptable worst-case tallies under this solution are:
+% 1. 4/6 - 4/6
+% 2. 4/6 - 3/3
+% 3. 4/6 - 0/0
+% These are ALL CORRECT.
+% At the other extreme, we have:
+% 4. 1/6 - 0/* , which are fine too.
+%@ N1 = 6,
+%@ T1 in 0..4,
+%@ T2 in 0..4,
+%@ N2#>=T2,
+%@ N2 in 3\/6 ;
+% Relative to the solution above, this solution appropriately
+% disallows 0/6 - 0/0, which is indeed undesirable as it sees
+% a full 0/6 toxicities at low dose without having escalated.
+%@ T2 = N2, N2 = 0,
+%@ T1 in 0..3,
+%@ N1#>=T1,
+%@ N1 in 0\/3 ;
+% What these cases allow is 0/0 - 0/0 (okay) and */3 - 0/0.
+% These are both fine, as they represent 'just starting'.
+%@ T1 = 0,
+%@ N1 = 3,
+%@ T2 in 0..4,
+%@ N2#>=T2,
+%@ N2 in 0\/3\/6.
+% These (finally) are the cases where 0/3 leads to escalation:
+% 0/3 - 0/0 or */3 or {0,1}/3+*/3.
+%% DONE: Examine the above for correctness.
 
 /*
 
 A DESIGN then consists of a set of arrows connecting TALLIES to
 TALLY SETS that are reachable with a single, allowable step.
+               vvvvv WRONG IDEA vvvvv
 Provided that 'acceptability' constitutes a PREFERENCE RELATION
 that is monotonic in toxicity (more toxicity is always worse),
 then I think we may WLOG draw the arrows simply to the WORST
 POSSIBLE OUTCOME TALLY.
+               ^^^^^ WRONG IDEA ^^^^^
+
+WAIT! Acceptability MAY VERY WELL NOT be fully monotonic in toxicity.
+For example, some very low tallies may indicate a failure to escalate
+fast enough. Accordingly, I require perhaps a more general notion of
+what a design is. Yes, this is true!
+
+So in fact I see CLP must ACTUALLY be brought into play SUBSTANTIVELY.
+This is a GOOD THING! But it does mean that my formulation remains 
+somewhat complex. The targets of arrows are perhaps mere denominators,
+but the CONDITIONS on VALID arrows are UNIVERSALLY QUANTIFIED.
+
+That is, an arrow is valid iff ALL POSSIBLE OUTCOME TALLIES are acceptable.
 
 Visualized as overlaid on the denominator lattice, then, the ARROWS
-OF A DESIGN map TALLIES --> TALLIES, indicating for each escalation
-decision the worst possible outcome. This is a STRONG FORMULATION
-that FROM THE OUTSET excludes arrows corresponding even to very rare
-catastrophic events. But this is just fine, since formulating the
-problem this way expresses OUR INTENT TO CONDUCT TRUE SAFETY ANALYSIS.
+OF A DESIGN map TALLIES --> DENOMINATORS, such that the denominator
+*difference* indicates for each escalation decision how many to enroll
+at each dose. (I am leaving open the possibility of mixed enrollment,
+to retain generality.) The valid arrows are those for which every possible
+end-tally is acceptable. Tallies that are reachable from a valid arrow,
+but which form the tail of no valid arrow, are terminal states of the
+design.
+
+If I wanted to obtain a true CATEGORY, I might do this by recognizing
+that in general the OBJECTS would be SETS OF TALLIES.
 
 Of note, while we do obtain a CATEGORY here, in which the tallies
 (T1/N1, T2/N2) are the OBJECTS and the steps are the ARROWS, unless
@@ -232,32 +269,26 @@ allowable_step(S1 - S2) :-
     S2 #>= 0,
     S12 #< 4. % let's say, 4+ DLTs in 1 step is unacceptable
 
-%?- denominator(N1a - N2a), denominator(N1b - N2b), N1b #= N1a + S1, N2b #= N2a + S2, S12 #= S1 + S2, S12 #< 4, S1 #>= 0, S2 #>= 0.
-%@ N1a in 0\/3\/6,
-%@ N1a+S1#=N1b,
-%@ S1 in 0..3,
-%@ S1+S2#=S12,
-%@ S2 in 0..3,
-%@ N2a+S2#=N2b,
-%@ N2a in 0\/3\/6,
-%@ N2b in 0\/3\/6,
-%@ S12 in 0..3,
-%@ N1b in 0\/3\/6.
-
 %?- allowable_step(A - B).
 %@ A in 0..3,
-%@ A+B#=_20888,
-%@ _20916+A#=_20912,
+%@ A+B#=_12040,
+%@ _12068+A#=_12064,
 %@ B in 0..3,
-%@ _20964+B#=_20960,
-%@ _20964 in 0\/3\/6,
-%@ _20960 in 0\/3\/6,
-%@ _20888 in 0..3,
-%@ _20916 in 0\/3\/6,
-%@ _20912 in 0\/3\/6.
-
-%% "Residual program"
-%% The answer is a transformation of the program!
+%@ _12116+B#=_12112,
+%@ _12116 in 0\/3\/6,
+%@ _12112 in 0\/3\/6,
+%@ _12040 in 0..3,
+%@ _12068 in 0\/3\/6,
+%@ _12064 in 0\/3\/6.
+% Interesting to appreciate that the unlabelled
+% residual program isn't too helpful for vetting
+% in this particular instance.
+%?- allowable_step(A - B), indomain(A), indomain(B).
+%@ A = B, B = 0 ; %% This is okay, if the identity arrow is useful.
+%@ A = 0,  %% This is the case of
+%@ B = 3 ; %% enrolling dose-level 2.
+%@ A = 3,  %% Enrolling
+%@ B = 0.  %% dose-level 1.
 
 step(T1a/N1a - T2a/N2a, T1b/N1b - T2b/N2b) :-
     denominator(N1a - N2a),
@@ -265,25 +296,199 @@ step(T1a/N1a - T2a/N2a, T1b/N1b - T2b/N2b) :-
     S1 #= N1b - N1a,
     S2 #= N2b - N2a,
     allowable_step(S1 - S2),
-    acceptable_tally(T1b/N1b - T2b/N2b),
-    acceptable_tally(T1a/N1a - T2a/N2a),
+    acceptable_tally(T1b - T2b, N1b - N2b),
+    acceptable_tally(T1a - T2a, N1a - N2a),
     true.
 
-%?- step(A, B).
+%?- step(0/3 - 0/0, T1b/N1b - T2b/N2b), indomain(N1b), indomain(N2b).
+%@ N1b = 6,
+%@ T2b = N2b, N2b = 0,
+%@ T1b in 1..4 ;
+%@ N1b = 6,
+%@ T2b = N2b, N2b = 0,
+%@ T1b in 1..4 ;
+%@ N1b = 3,
+%@ T2b = N2b, N2b = 0,
+%@ T1b in 0..3 ;
+%@ N1b = 3,
+%@ T2b = N2b, N2b = 0,
+%@ T1b in 0..3 ;
+%@ T1b = T2b, T2b = N2b, N2b = 0,
+%@ N1b = 3 ;
+%@ T1b = 0,
+%@ N1b = N2b, N2b = 3,
+%@ T2b in 0..3 ;
+%@ T1b = T2b, T2b = N2b, N2b = 0,
+%@ N1b = 3 ;
+%@ T1b = 0,
+%@ N1b = N2b, N2b = 3,
+%@ T2b in 0..3.
 
-/* - - - -
-%% DONE: Read memoization in Markus's book
-:- dynamic memo/1.
-memo(Goal) :- ( Goal -> true ; Goal, assertz(Goal)).
-
-%% Dangerous territory!
-:- dynamic memo_/1.
-memo(Goal) :-
-( memo_(Goal) -> true
-; once(Goal),
-assertz(memo_(Goal))
-).
+/*
+Interesting! What we see here is a set of steps that lead to acceptable tallies.
+But what is not checked here is whether the chosen end-denominator ensures that
+ALL POSSIBLE end-tallies are acceptable! The latter is a much stronger condition,
+and is what in fact yields the USEFUL inference about the CONTENT of a design.
 */
+
+
+%?- step(T1a/N1a - T2a/N2a, T1b/N1b - T2b/N2b), indomain(N1a), indomain(N2a), indomain(N1b), indomain(N2b).
+%@ N1a = N1b, N1b = 6,
+%@ T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ T1a in 1..4,
+%@ T1b in 1..4 ;
+%@ N1a = N1b, N1b = 6,
+%@ T2a = N2a, N2a = 0,
+%@ N2b = 3,
+%@ T1a in 1..4,
+%@ T1b in 1..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = 6,
+%@ N2a = N2b, N2b = 3,
+%@ T1a in 1..4,
+%@ T2a in 0..3,
+%@ T1b in 1..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = N2b, N2b = 6,
+%@ N2a = 3,
+%@ T1a in 1..4,
+%@ T2a in 0..3,
+%@ T1b in 1..4,
+%@ T2b in 0..4 ;
+%@ N1a = N2a, N2a = N1b, N1b = N2b, N2b = 6,
+%@ T1a in 1..4,
+%@ T2a in 0..4,
+%@ T1b in 1..4,
+%@ T2b in 0..4 ;
+%@ N1a = N1b, N1b = 6,
+%@ N2a = N2b, N2b = 3,
+%@ T1a in 0..4,
+%@ T2a in 0..3,
+%@ T1b in 1..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = N2b, N2b = 6,
+%@ N2a = 3,
+%@ T1a in 0..4,
+%@ T2a in 0..3,
+%@ T1b in 1..4,
+%@ T2b in 0..4 ;
+%@ N1a = N2a, N2a = N1b, N1b = N2b, N2b = 6,
+%@ T1a in 0..4,
+%@ T2a in 0..4,
+%@ T1b in 1..4,
+%@ T2b in 0..4 ;
+%@ N1a = 3,
+%@ T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ N1b = 6,
+%@ T1a in 0..3,
+%@ T1b in 1..4 ;
+%@ T1a = T2a, T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ N1a = 3,
+%@ N1b = 6,
+%@ T1b in 1..4 ;
+%@ T1a = 0,
+%@ N1a = N2a, N2a = N2b, N2b = 3,
+%@ N1b = 6,
+%@ T2a in 0..3,
+%@ T1b in 1..4,
+%@ T2b in 0..3 ;
+%@ T1a = 0,
+%@ N1a = 3,
+%@ N2a = N1b, N1b = N2b, N2b = 6,
+%@ T2a in 0..4,
+%@ T1b in 1..4,
+%@ T2b in 0..4 ;
+%@ N1a = N1b, N1b = 6,
+%@ T2a = N2a, N2a = 0,
+%@ N2b = 3,
+%@ T1a in 1..4,
+%@ T1b in 0..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = 6,
+%@ N2a = N2b, N2b = 3,
+%@ T1a in 1..4,
+%@ T2a in 0..3,
+%@ T1b in 0..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = N2b, N2b = 6,
+%@ N2a = 3,
+%@ T1a in 1..4,
+%@ T2a in 0..3,
+%@ T1b in 0..4,
+%@ T2b in 0..4 ;
+%@ N1a = N2a, N2a = N1b, N1b = N2b, N2b = 6,
+%@ T1a in 1..4,
+%@ T2a in 0..4,
+%@ T1b in 0..4,
+%@ T2b in 0..4 ;
+%@ N1a = N1b, N1b = 6,
+%@ N2a = N2b, N2b = 3,
+%@ T1a in 0..4,
+%@ T2a in 0..3,
+%@ T1b in 0..4,
+%@ T2b in 0..3 ;
+%@ N1a = N1b, N1b = N2b, N2b = 6,
+%@ N2a = 3,
+%@ T1a in 0..4,
+%@ T2a in 0..3,
+%@ T1b in 0..4,
+%@ T2b in 0..4 ;
+%@ N1a = N2a, N2a = N1b, N1b = N2b, N2b = 6,
+%@ T1a in 0..4,
+%@ T2a in 0..4,
+%@ T1b in 0..4,
+%@ T2b in 0..4 ;
+%@ T1a = 0,
+%@ N1a = N2a, N2a = N2b, N2b = 3,
+%@ N1b = 6,
+%@ T2a in 0..3,
+%@ T1b in 0..4,
+%@ T2b in 0..3 ;
+%@ T1a = 0,
+%@ N1a = 3,
+%@ N2a = N1b, N1b = N2b, N2b = 6,
+%@ T2a in 0..4,
+%@ T1b in 0..4,
+%@ T2b in 0..4 ;
+%@ T1a = N1a, N1a = T2a, T2a = N2a, N2a = T1b, T1b = N1b, N1b = T2b, T2b = N2b, N2b = 0 ;
+%@ T1a = N1a, N1a = T2a, T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ N1b = 3,
+%@ T1b in 0..3 ;
+%@ N1a = N1b, N1b = 3,
+%@ T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ T1a in 0..3,
+%@ T1b in 0..3 ;
+%@ T1a = T2a, T2a = N2a, N2a = T2b, T2b = N2b, N2b = 0,
+%@ N1a = N1b, N1b = 3,
+%@ T1b in 0..3 ;
+%@ T1a = N1a, N1a = T2a, T2a = N2a, N2a = T1b, T1b = T2b, T2b = N2b, N2b = 0,
+%@ N1b = 3 ;
+%@ N1a = N1b, N1b = 3,
+%@ T2a = N2a, N2a = T1b, T1b = T2b, T2b = N2b, N2b = 0,
+%@ T1a in 0..3 ;
+%@ N1a = N1b, N1b = N2b, N2b = 3,
+%@ T2a = N2a, N2a = T1b, T1b = 0,
+%@ T1a in 0..3,
+%@ T2b in 0..3 ;
+%@ T1a = T2a, T2a = N2a, N2a = T1b, T1b = T2b, T2b = N2b, N2b = 0,
+%@ N1a = N1b, N1b = 3 ;
+%@ T1a = T2a, T2a = N2a, N2a = T1b, T1b = 0,
+%@ N1a = N1b, N1b = N2b, N2b = 3,
+%@ T2b in 0..3 ;
+%@ T1a = T1b, T1b = 0,
+%@ N1a = N2a, N2a = N1b, N1b = N2b, N2b = 3,
+%@ T2a in 0..3,
+%@ T2b in 0..3 ;
+%@ T1a = T1b, T1b = 0,
+%@ N1a = N2a, N2a = N1b, N1b = 3,
+%@ N2b = 6,
+%@ T2a in 0..3,
+%@ T2b in 0..4 ;
+%@ T1a = T1b, T1b = 0,
+%@ N1a = N1b, N1b = 3,
+%@ N2a = N2b, N2b = 6,
+%@ T2a in 0..4,
+%@ T2b in 0..4.
 
 %% Big questions: what are we talking about at all?
 
