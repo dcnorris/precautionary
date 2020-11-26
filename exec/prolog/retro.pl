ebfc14caf3c2b1eea12e1125b52185b70ae64a63
David Norris <david@UnusualSolutionsThatWork.com>
2020-11-25 22:04:27 -0500

    Lots of progress, what can I say?

--- exec/prolog/retro.pl
+++ exec/prolog/retro.pl
@@ -1,3 +1,8 @@
+% Prefix op * for 'generalizing away' goals (https://www.metalevel.at/prolog/debugging)
+:- op(920, fy, *). *_.  % *Goal always succeeds
+
+:- use_module(library(clpfd)).
+
 /* - - -
 
 Toward a declarative DSL for dose-escalation trial design...
@@ -192,4 +197,391 @@ mtd_escalation(MTD, Escalation) :-
     ),
     nth1(MTD+1, Tallies, T/_),  % ..AND for which the next-higher dose
     T #> 2,                     % has shown at least 2 toxicities
-    N #<= 6.                    % out of no more than 6 patients.
+    N #=< 6.                    % out of no more than 6 patients.
+
+/* - - - -
+
+Moving past pseudocode, let's try to implement these ideas in a 2-dose setting.
+
+1. First thing I could use is a DCG to generate all possible enrollment strings.
+
+This ought to represent only the most elemental aspects of dose-escalation.
+For example, the fact that the next dose is always at most 1 higher than
+the present dose -- or, to begin this exploration, that we always move up
+OR down by 1 dose.
+
+2. Superimposed on this may be some sense that our LEARNING (i.e., the trial's
+PROGRESS toward its DRUG-DEVELOPMENT AIMS) manifests solely in the up-and-down
+of dose-escalation itself.
+
+Seen from a PC perspective, of course, this is utterly false! But I do believe
+that the logic underlying dose-escalation IN PRACTICE adheres to this idea,
+however bad that may be. So REPRESENTING THIS IDEA OBJECTIVELY is useful.
+
+===
+Here's the SCIENTIFIC QUESTION I see motivating the next step of this effort:
+How far toward a full design can we proceed, simply by taking a few notional
+examples of dose-escalation decisions, and 'processing' them through the logic
+of DOSE-ESCALATION-AS-LEARNING?
+
+I'm actually going to call that 'DEAL' ;^), and perhaps it turns out to be a
+valuable contribution of this effort that it explores the consequences and
+limitations of the DEAL principle.
+
+ - - - - -*/
+
+%% I begin by modeling a dose-escalation ...
+%% AHA! It looks to me as if I must carry along the TALLIES as CONTEXT
+%% for this DCG. Is this correct? Is there some other way?
+%% It seems I am constantly re-learning how to use DCG state vs semicontext!
+%% Intuitively--based on the SYNTAX, really!--, I feel that 'lookahead'
+%% (maybe with 'replacement') is appropriate for knowing what the last
+%% enrolled dose was. OTOH, it does seem as if the current TALLIES have
+%% to be managed as STATE.
+dose_esc([0/0, 0/0, 0/0]) --> [1 - Tox], { Tox in 0..1 }.
+dose_esc(Tallies_1), [Dose_1 - Tox_1] -->
+                     [Dose_1 - Tox_1, Dose - Tox],
+		     { length(D, Tallies_1),
+		       Dose in 1..D,
+		       Dose #>= Dose_1 - 1, % Next dose in DE always stays
+		       Dose #=< Dose_1 + 1, % within 1 level of previous dose.
+		       Tox in 0..1 }, % TODO: Eventually, CTCAE Grades 0..5
+		     tallies0_assessment_tallies(Tallies_1, Dose - Tox, Tallies),
+		     dose_esc(Tallies).
+dose_esc(Tallies) --> [declare_mtd(MTD)],
+		      { mtd_tallies(MTD, Tallies) }.
+
+%% 'Union' or 'sum' relation on tallies lists: Tallies0 + Tox@Dose = Tallies.
+/*
+This has become a problem almost immediately! I ought to be able to access
+and set elements of a tallies list without so much trouble!
+TODO: Switch to something like keypairs lists?
+ */
+tallies0_assessment_tallies(Tallies0, Dose - Tox, Tallies) :-
+    (	nth1(Dose, Tallies0, T0/N0, Remainder),
+	N #= N0 + 1,
+	T #= T0 + Tox,
+	nth1(Dose, Tallies, T/N, Remainder)
+    ;	append(Tallies0, [Tox/1], Tallies),
+	length(Tallies, Dose) % w/o dose-skipping, length(Tallies0, Dose-1) holds.
+    ).
+
+%?- tallies0_assessment_tallies([0/0,0/0,0/0], 2 - 0, Tallies).
+%@ Tallies = [0/0, 0/1, 0/0] ;
+%@ false.
+%@ Tallies = [0/0, 1/1, 0/0] ;
+%@ false.
+%@ Tallies = [_6368, 1/1|_6376] ;
+%@ Action (h for help) ? Unknown option (h for help)
+%@ Action (h for help) ? abort
+%@ % Execution Aborted
+%@ ERROR: Unknown procedure: tallies0_assessment_tallies/3 (DWIM could not correct goal)
+
+%% I'll let tallies be positional LISTS, effectively defaulting to 0/0:
+tallies_dose_tally(Tallies, D, T/N) :-
+    (	nth1(D, Tallies, T/N)
+    ;	length(Tallies, L),
+	D #> L,
+	T/N = 0/0
+    ).
+
+%% MGQ
+%?- tallies_dose_tally(Tallies, Dose, Tally).
+%@ Tallies = [_2884/_2886|_2898],
+%@ Dose = 1,
+%@ Tally = _2884/_2886 ;
+%@ Tallies = [_2896, _2884/_2886|_4332],
+%@ Dose = 2,
+%@ Tally = _2884/_2886 ;
+%@ Tallies = [_2896, _4330, _2884/_2886|_5766],
+%@ Dose = 3,
+%@ Tally = _2884/_2886 ;
+%@ Tallies = [_2896, _4330, _5764, _2884/_2886|_7200],
+%@ Dose = 4,
+%@ Tally = _2884/_2886 ... YEP!
+
+/*
+TODO: Show that it's possible to declare MTD upon seeing 2/2 toxicities,
+      without filling the full 'cohort of 3' DESPITE the fact that the
+      MTD criterion may be EXPRESSED AS 2/3. That is, show that the logic
+      of dose-escalation enables the DSL to 'think several moves ahead'.
+
+This might be as simple as the brute-force step of calculating, from any
+stage of a partially realized escalation, what possible outcomes there
+might be. When the set of possible MTD declarations becomes singular,
+then just declare now.
+
+That logic may also be extended (probably more feasibly, too) to the next
+enrollment dosing decision. If that decision would be unaffected by some
+given present enrollment decision, then the present decision is 'wrong'
+in a sense.
+
+*/
+
+/* - - - - -
+
+AHA! Here is the RECURSIVE concept I have been waiting for! I ought to
+calculate the number of possible escalations FORWARD FROM A GIVEN TALLY
+that results in a given outcome, such as declare_mtd(3).
+
+This quantity can be calculated RECURSIVELY and (probably) allows injection
+of DYNAMIC-PROGRAMMING ideas. With any luck (or maybe a lot of it!), the
+dynamic-programming approach (plus tabling?) may allow me to circumvent an
+otherwise impure, forall-based approach to this problem.
+
+What does this circumvention depend on? Ultimately, it depends on a base
+case that can be aggregated through the search tree.
+
+PLAN:
+/1 Implement stricter tally comparisons &<, &>, &=, etc.
+   where the comparison is conceptually grounded in the
+   sequences of (0/1 | 1/1) that sum up to them.
+2) Implement relation {TallyList - MTD}
+3) Relate TallyLists to Escalations that generate them.
+   Initially, this can be *ANY* Escalation sequence consistent
+   with some DCG. I can leave 'for later' the culling of these
+   many candidate Escalations, according to DEAL and other
+   principles.
+4) Implement relation {TallyList - Escalation - MTD}
+5) Render an 'abstract interpretation' of the above relation,
+   concerned strictly with the LENGTH of the Escalation needed
+   to reach the given MTD, or at least SOME QUANTITY THAT CAN
+   BE CALCULATED RECURSIVELY on a tree. This could be, e.g.,
+   the minimum number of patients needed to enroll to reach
+   an MTD declaration.
+
+I have to remember the IMPORTANCE of such a recursive calculation
+derives from its potential to accomplish a forall/3 or \+ BY PURE
+MEANS!
+
+What happens at the point of IMPLEMENTATION is the {TallyList - MTD}
+relation serves as the base case for {TallyList - Escalation - MTD},
+where Escalation = []. Another case deals with the VERY FINITE case
+where Escalation = [Dose - Tox] for a single patient. I can probably
+deal with this 'in closed form' (in some sense I don't yet understand
+fully) so that I can obtain a number. At each node running backwards
+toward the root of the tree, I will obtain a new summary. The forall
+or \+ happens when I find I've counted ZERO THINGS HAPPENING, say.
+
+It might even be the case that I will require MULTIPLE BINARY OPS
+in order to fully characterize the 'answer to the question' at any
+given point during the trial. Maybe {max, min, and} are all needed!
+Ideally, I would parametrize such ops, for accumulation within the
+tree, by a generic maplist-like predicate.
+
+** THE MOST IMPORTANT THINKING to do at this point is to understand
+** what sort of \+iness I need to implement in 'deducing' the trial.
+
+CAPTURING *PROGRESS* VIA DYNAMIC PROGRAMMING
+
+I think that DP promises to make a contribution mainly in defining
+a notion of 'progress' for dose-escalation trials, such that next
+enrollment dose could be chosen on a maximin or minimax criterion
+that let the next dose chosen be the highest one that was sure to
+reduce the number of steps to MTD.
+
+ - - - - - - */
+
+% This is true if, starting from Tallies a *further* Escalation sequence
+% starting from Dose results in declaration of MTD.
+
+% The base case for this recursion defines what tallies yield an MTD right away.
+% Note that Dose is anonymous in this clause because MTD declaration depends
+% only on the net Tallies seen so far.
+/*
+tallies_dose_escalation_mtd(Tallies, _, [], MTD) :-
+    true.
+tallies_dose_escalation_mtd(Tallies, Dose, Escalation, MTD) :-
+    true.
+*/
+
+/*
+Something that ought to be kept in mind is that these rather strict
+inequalities tend to undervalue the information from larger cohorts.
+This tendency 'tips the scales' in favor of changing the dose earlier
+rather than later. I need to be on guard against having hereby imposed
+a tendency that deserved to make a more spontaneous/organic entrance.
+*/
+%?- 4/9 &>= 2/6.
+%@ false.
+
+%?- maplist(lowtally, []).
+%@ true.
+
+%?- lowtally(0/3).
+%@ false.
+
+%?- tally(0/3).
+%@ true.
+
+%?- 0/3 &=< 0/3.
+%@ true ;
+%@ false.
+
+%?- 0/3 &=< 0/3.
+%@ true ;
+%@ false.
+
+
+
+%?- lowtally_(1, 0/3).
+%@ true.
+%@ false.
+%@ true.
+
+%?- maplist(lowtally, [0/3, 0/6]).
+%@ false.
+%@ false.
+%@ true ;
+%@ false.
+%@ true ;
+%@ true.
+
+%?- T/N &>= 1/3.
+%@ T in 1..sup,
+%@ N#>=T,
+%@ N in 1..sup.
+
+%?- 1/10 &>= 1/3.
+%@ false.
+
+%?- tallylist_mtd(Tallies, 0).
+%@ Tallies = [_928590/_928592|_928586],
+%@ _928590 in 1..3,
+%@ _928592#>=_928590,
+%@ _928592 in 1..3 ;
+%@ Tallies = [_931550/_931552|_931546],
+%@ _931550 in 2..sup,
+%@ _931552#=<_931550+2,
+%@ _931552#>=_931550,
+%@ _931552 in 4..sup ;
+%@ Tallies = [_936246/_936248|_936242],
+%@ _936246 in 2..6,
+%@ _936248#>=_936246,
+%@ _936248 in 2..6 ;
+%@ Tallies = [_939206/_939208|_939202],
+%@ _939206 in 3..sup,
+%@ _939208#=<_939206+4,
+%@ _939208#>=_939206,
+%@ _939208 in 7..sup ;
+%@ Action (h for help) ? abort
+%@ % Execution Aborted
+
+%?- 0/3 &>= 0/99.
+%@ true.
+
+%?- 0/99 &=< 0/3.
+%@ true.
+
+%?- append(L, M, [1,2,3,4,5]).
+%@ L = [],
+%@ M = [1, 2, 3, 4, 5] ;
+%@ L = [1],
+%@ M = [2, 3, 4, 5] ;
+%@ L = [1, 2],
+%@ M = [3, 4, 5] ;
+%@ L = [1, 2, 3],
+%@ M = [4, 5] ;
+%@ L = [1, 2, 3, 4],
+%@ M = [5] ;
+%@ L = [1, 2, 3, 4, 5],
+%@ M = [] ;
+%@ false.
+
+%?- maplist(#<(1), [2,3,4]).
+%@ true.
+%@ false.
+%@ ERROR: Syntax error: Unbalanced operator
+%@ ERROR: maplist(1 #
+%@ ERROR: ** here **
+%@ ERROR: <, [2,3,4]) . 
+
+% What does a DLT tally look like?
+tally(DLTs/Enrolled) :-
+    Enrolled in 1..sup,
+    0 #=< DLTs, DLTs #=< Enrolled.
+
+%?- tally(T/N).
+%@ T in 0..sup,
+%@ N#>=T,
+%@ N in 1..sup.
+
+%% Ooh.. look at this! I need to delve a bit into the details
+%% of comparing tallies. I rather think that some comparisons
+%% ought to be recognized as indeterminate. Perhaps a tally
+%% such as 0/2 is INCOMMENSURATE with 1/3! How do you know
+%% that the 0/2 won't become a 1/3 with the next patient?
+/*
+Would the SETS of dose-escalations that correspond to these
+tallies provide a sounder basis for thinking about such
+comparisons?
+
+Or maybe worst-case scenarios? I can say that 1/2 &=< 2/3
+because the possible outcomes are 1/2 --> {2/3, 1/3}, a set
+that is at worst equal to 2/3 and possibly better.
+
+Do the complementary-looking comparisons such as [&< and &>=]
+lose some of their complementarity under such an understanding?
+Or do I merely eject many pairs of tallies from all comparisons?
+*/
+%?- 0/2 &< 1/3.
+%@ true.
+
+:- op(900, xfx, user:(&>=)). % '_ can't be(come) any better than _'
+&>=(T1/N1, T2/N2) :-
+    tally(T1/N1),
+    tally(T2/N2),
+    DT #= max(0, N1 - N2),
+    T1 #>= T2 + DT.
+
+:- op(900, xfx, user:(&=<)). % '_ can't be(come) any worse than _'
+&=<(Q1, Q2) :- Q2 &>= Q1.
+
+%?- 0/2 &>= 0/3.
+%@ true.
+
+%?- X #= max(0,2).
+%@ X = 2.
+
+%?- T/N &>= 1/3, T in 0..5, N in 0..5.
+%@ T in 1..5,
+%@ N in 0..3 ;
+%@ T in 2..5,
+%@ N#=<T+2,
+%@ N in 4..5.
+
+%% Define &= as conjunction of &>= with &=<.
+:- op(900, xfx, user:(&=)).
+&=(Q1, Q2) :- Q1 &>= Q2, Q2 &>= Q1.
+
+%% Demonstrate that &= as above requires STRICT EQUALITY
+%% of BOTH numerator & denominator:
+%?- T1/N1 &= T2/N2, (T1 #\= T2; N1 #\= N2), [T1, T2, N1, N2] ins 0..6, label([T1,T2,N1,N2]).
+%@ false.
+
+%% TODO: Consider defining other comparisons, including &\=
+%%       and strict inequalities &< and &>. Learning where,
+%%       how and why the various possible tally comparisons
+%%       do or--especially!--DO NOT prove useful may itself
+%%       yield insights.
+
+lowtally(Q) :-
+    tally(Q),
+    %(	Q &=< 0/3
+    %;	Q &=< 1/6
+    %).
+    Q &=< 0/3 #<==> B,
+    lowtally_(B, Q).
+
+lowtally_(1, _).
+lowtally_(0, Q) :- Q &=< 1/6.
+
+tallylist_mtd(Tallies, MTD) :-
+    append(LowTallies, [T/N | _], Tallies),
+    length(LowTallies, MTD),
+    maplist(lowtally, LowTallies),
+    tally(T/N),
+    (	T/N &>= 1/3
+    ;	T/N &>= 2/6
+    ).
+
