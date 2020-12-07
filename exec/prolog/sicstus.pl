f3da775e690b791d13d3089c8c8d70e366c12222
David Norris <david@UnusualSolutionsThatWork.com>
2020-12-07 11:20:42 -0500

    Add SICStus target to sysdata make

--- exec/prolog/sicstus.pl
+++ exec/prolog/sicstus.pl
@@ -1,8 +1,12 @@
 :- use_module(library(clpfd)).
+:- use_module(library(lists)).
 
 % Approximate format_//2 as provided by Scryer's library(format):
+:- use_module(library(codesio)).
+
 format_(Fs, Args) --> call(format__(Fs, Args)).
 
 format__(Fs, Args, Ls0, Ls) :-
-    format(chars(Ls0,Ls), Fs, Args).
-
+    with_output_to_codes(format(Fs, Args), Codes0, Ls),
+    name(Atom0, Codes0),
+    atom_chars(Atom0, Ls0).
