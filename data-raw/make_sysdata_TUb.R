efdc914595699634d2b6989ee71518d0fc2896b3
David Norris <david@UnusualSolutionsThatWork.com>
2020-12-08 17:08:55 -0500

    Rename array 'A' to 'T', matching paper

--- data-raw/make_sysdata_TUb.R
+++ data-raw/make_sysdata_TUb.R
@@ -1,18 +1,18 @@
 ## Make the internal data {A, U, b} required for exact simulation of 3+3 design
 
-A <- U <- b <- list()
+T <- U <- b <- list()
 for(D in 2:8){
-  T <- read.table(paste0("T", D, ".tab"))
-  J <- nrow(T)/2
-  A[[D]] <- aperm(array(as.matrix(T), dim = c(2, J, D)
+  T_raw <- read.table(paste0("T", D, ".tab"))
+  J <- nrow(T_raw)/2
+  T[[D]] <- aperm(array(as.matrix(T_raw), dim = c(2, J, D)
                         , dimnames = list(c = 1:2, j = NULL, d = paste0("D",1:D)))
                   , perm = c("c","d","j"))
-  U[[D]] <- cbind(apply(A[[D]], c("j","d"), sum, na.rm=TRUE),
-                  apply(3 - A[[D]], c("j","d"), sum, na.rm=TRUE))
+  U[[D]] <- cbind(apply(T[[D]], c("j","d"), sum, na.rm=TRUE),
+                  apply(3 - T[[D]], c("j","d"), sum, na.rm=TRUE))
   dimnames(U[[D]]) <- list(j = NULL, pq = c(paste0("D", rep(1:D, 2))))
-  b[[D]] <- apply(log(choose(3, A[[D]])), "j", sum, na.rm=TRUE)
+  b[[D]] <- apply(log(choose(3, T[[D]])), "j", sum, na.rm=TRUE)
 }
 
-usethis::use_data(A, U, b,
+usethis::use_data(T, U, b,
                   internal = TRUE,
                   overwrite = TRUE)
