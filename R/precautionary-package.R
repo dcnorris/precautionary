50e3be1c5100df5d5866c2c31aff236c085ef28e
David Norris <david@UnusualSolutionsThatWork.com>
2021-01-12 16:15:48 -0500

    v0.2-1 as submitted to CRAN

--- R/precautionary-package.R
+++ R/precautionary-package.R
@@ -7,75 +7,75 @@
 #' @aliases precautionary-package precautionary
 #' @docType package
 #' @author David C. Norris (<https://orcid.org/0000-0001-9593-6343>)
-#' 
+#'
 #' @references
 #' 1. Norris DC. Dose Titration Algorithm Tuning (DTAT) should supersede
 #'    \sQuote{the} Maximum Tolerated Dose (MTD) in oncology dose-finding trials.
 #'    \emph{F1000Research}. 2017;6:112. \doi{10.12688/f1000research.10624.3}.
 #'    \url{https://f1000research.com/articles/6-112/v3}
-#' 
+#'
 #' 2. Norris DC. Costing \sQuote{the} MTD. \emph{bioRxiv}. August 2017:150821.
 #'    \doi{10.1101/150821}.
 #'    \url{https://www.biorxiv.org/content/10.1101/150821v3}
-#' 
+#'
 #' 3. Norris DC. Precautionary Coherence Unravels Dose Escalation Designs.
 #'    \emph{bioRxiv}. December 2017:240846. \doi{10.1101/240846}.
 #'    \url{https://www.biorxiv.org/content/10.1101/240846v1}
-#' 
+#'
 #' 4. Norris DC. One-size-fits-all dosing in oncology wastes money, innovation
 #'    and lives. \emph{Drug Discov Today}. 2018;23(1):4-6.
 #'    \doi{10.1016/j.drudis.2017.11.008}.
 #'    \url{https://www.sciencedirect.com/science/article/pii/S1359644617303586}
-#' 
+#'
 #' 5. Norris DC. Costing \sQuote{the} MTD ... in 2-D. \emph{bioRxiv}. July 2018:370817.
 #'    \doi{10.1101/370817}.
 #'    \url{https://www.biorxiv.org/content/10.1101/370817v1}
-#' 
+#'
 #' 6. Norris DC. Ethical Review and Methodologic Innovation in Phase 1 Cancer Trials.
 #'    \emph{JAMA Pediatrics}. 2019;173(6):609
 #'    \doi{10.1001/jamapediatrics.2019.0811}.
-#'    
+#'
 #' 7. Norris DC. Comment on Wages et al., Coherence principles in interval-based
 #'    dose finding. Pharmaceutical Statistics 2019, DOI: 10.1002/pst.1974.
 #'    \emph{Pharmaceutical Statistics}. March 2020.
 #'    \doi{10.1002/pst.2016}.
-#'    
+#'
 #' 8. Norris DC. Retrospective analysis of a fatal dose-finding trial.
 #'    arXiv:2004.12755 \[stat.ME\]. April 2020.
 #'    \url{https://arxiv.org/abs/2004.12755}
-#' 
+#'
 #' 9. Norris DC. What Were They Thinking? Pharmacologic priors implicit in a choice
 #'    of 3+3 dose-escalation design. arXiv:2012.05301 \[stat.ME\]. December 2020.
 #'    \url{https://arxiv.org/abs/2012.05301}
-#' 
+#'
 #' @import methods
 #' @import magrittr
 #' @import data.table
 NULL
 
 #' Ordinalizers
-#' 
+#'
 #' TODO: Explain ordinalization and ordinalizers here.
-#' 
+#'
 #' @section Limitations:
 #' TODO: Be sure to note the highly restrictive assumptions made about
 #' ordinalizer functions, especially as noted e.g. in connection with
 #' the internal function G (defined in 'exact.R').
-#' 
+#'
 #' @section Validity:
 #' The validity of an ordinalizer might well be programmatically testable.
 #' If so, all this discussion might well be carried out in documentation
 #' for a validity-testing function.
-#' 
+#'
 #' @name ordinalizer
 #' @aliases ordinalizers ordinalization
 NULL
 
 #' Plan
-#' 
+#'
 #' @section Document:
-#' * Add a proper vignette detailing and exploring exact 3+3 calculations
-#' 
+#' * Add a vignette applying DTP to mTPI and BOIN
+#'
 #' @section Refactor:
 #' * Remove the `$safety` component of exact trials?
 #'   - Perhaps this ought to be calculated 'on the fly' by the summary method.
@@ -84,7 +84,7 @@ NULL
 #' * Should summary(EXACT)$safety bear class 'safetytab'?
 #' * Example for 'as.data.table.exact'
 #' * What is role of G function in exact.R?
-#' 
+#'
 #' @section Extend:
 #' * Implement exact 3+3 variant with \code{allow_deescalation=FALSE}
 #' * Implement rolling 6
@@ -94,10 +94,10 @@ NULL
 #'     of this software design.
 #' * Index `sims$fits` to exact outcomes in `A[[D]]` where appropriate
 #'   - See the `haystack` function in `exact.R`
-#' 
+#'
 #' @section Robustify:
 #' * Tests comparing results from multiple CRAN packages
-#' 
+#'
 #' @name plan
 #' @aliases todo
 NULL
--- cran-comments.md
+++ cran-comments.md
@@ -1,11 +1,6 @@
 ## Test environments
 * local OS X install, R 4.0.3
-* win-builder (devel)
+* rhub::check(platform="windows-x86_64-devel")
 
 ## R CMD check --as-cran results
-There were no (non-transient) ERRORs or WARNINGs.
-
-Previous NOTE about Non-standard file/directory
-    found at top level: 'data-raw'
-has been dealt with by moving code to exec/,
-so that it is distributed with the package.
+There were no NOTEs, ERRORs or WARNINGs.
