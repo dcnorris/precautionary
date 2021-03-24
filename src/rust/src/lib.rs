d0b4989d0e438de3b2a2dcc9468bc5fdad4fda80
David Norris <david@UnusualSolutionsThatWork.com>
2021-03-24 13:45:08 -0400

    Reduce crmh_ to crmh_non; implement w!=1 case

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -3,73 +3,30 @@ use extendr_api::prelude::*;
 #[allow(non_snake_case)]
 
 #[inline(always)]
-fn crmh_(a: &f64, // NB: *NOT* vectorized on a
-	 lnX: &[f64],
-	 D: usize,
-	 Y: &[f64],
-	 notox: &[i32],
-	 w: &[f64],
-	 s: f64,
-	 b: i32) -> f64 {
-    // Below was the original, very short computation.
-    // I count N+1 transcendental ops, compared with 2D+1 in new implementation.
-    // If an average of 4 patients are enrolled per dose level, this translates
-    // to a savings of (4D - 2D)/(4D+1) ~ 50% of effort.
-    // TODO: By precalculating the X[d].ln()'s in the calling function, the cost
-    //       of these txops amortizes to zero! Thus we can bring this routine
-    //       down to just D+1 transcendendal ops, saving 75% of effort!
-    //       I might even dare to call these ln()'s the EXOSKELETON.
-    /*
-    let mut v = a.powi(b) * (-0.5*(a/s).powi(2)).exp();         // 1 exp()
-    if v.is_infinite() { return 0.0; }
-    for i in 0 .. y.len() {
-	let p_i = x[i].powf(a.exp()); // 'power model' CRM      // N powf()'s
-	v = v * if y[i] == 0 { 1.0 - w[i] * p_i } else { p_i };
-    }
-    v
-     */
-
-    // I'm going to undertake some refactoring toward an algorithm
-    // that reduces transcendental ops to a bare minimum. The key
-    // opportunity in this regard lies in COMPUTING x[i]^exp_a_
-    // JUST ONCE PER DOSE LEVEL, rather than once per patient.
-    // Some bookkeeping preliminaries are needed to make this possible.
-    // As part of my initial refactoring, I will do this initially here
-    // in this routine; but this computation is one that ought to be
-    // bubbled upward as far as possible.
-
-    if a > &709.0 { return 0.0 } // "Not gonna exp() it; wouldn't be prudent."
-    let exp_a_ = a.exp();                                             // 1 exp()
-
-    // The objective function can factored as: v = vconst * log_vtox.exp() * v_non.
-    // (Let's call log_vtox the 'toxic term' and v_non the 'non-tox factor'.)
-    let log_vconst = -0.5 * (a/s).powi(2);
-    if log_vconst > 709.0 { return 0.0; } // saves time, avoids returning Inf*0=NaN
-
-    let mut log_vtox = 0.0;
-    for d in 0 .. D { // TODO: Use an iterator-based expression?
-	log_vtox += Y[d]*lnX[d];
-    }
-    log_vtox = log_vtox * exp_a_; // this completes the toxic term
-
-    // The non-tox factor is more difficult, because ln does not
-    // commute with any operations inside (1 - w * x^exp_a_) 8^(.
-    // So we must iterate over the patients. BUT... this can be
-    // done without racking up per-patient transcendental ops,
+fn crmh_non(exp_a_: &f64,  // NB: *NOT* vectorized
+	    D: usize,      // number of doses tried so far with any non-tox
+	    lnX: &[f64],   // log-skeleton at the no-tox doses
+	    notox: &[usize], // numbers of no-tox obs at doses lnX
+	    w: &Vec<f64>) -> f64 {
+    // The non-tox factor is the nontrivial part of the CRM power model integrand,
+    // because ln does not commute with any operations inside (1 - w * x^exp_a_).
+    // So we must iterate at least over the weights w[.] for patients without tox.
+    // BUT... this can be done without racking up per-patient transcendental ops,
     // provided we collect our factor multiplicatively (not by logs).
-    // The key point is that the following computation costs only
-    // X.len() transcendental ops -- just 1 powf() per dose tried.
     let mut v_non = 1.0; // to build non-tox factor by straight multiplication
+    let mut i: usize = 0; // will use this index to step thru w[i]
     for d in 0 .. D { // For each dose X[d] that was tried
-	let p_d = (lnX[d]*exp_a_).exp();                              // D powf()'s
-	for i in 0 .. notox[d] {
-	    v_non *= (1.0 - 1.0*p_d) // TODO: support general case as below:
-	    //v_non *= (1.0 - w[i]*p_d)
+	let p_d = (lnX[d]*exp_a_).exp();
+	// TODO: Would an into_iter of w be the more idiomatic expression?
+	//       Certainly, what I am doing is 'consuming' w, but in short
+	//       bursts that are interrupted by fresh p_d calculations.
+	for _ in 0 .. notox[d] {
+	    v_non *= 1.0 - w[i]*p_d;
+	    i += 1; // step to next weight
 	}
     }
 
-    let vfast = a.powi(b) * (log_vconst + log_vtox).exp() * v_non;    // 1 exp()
-    vfast
+    v_non
 }
 
 // Vectorize crmh1 on the 'a' parameter
@@ -88,15 +45,13 @@ fn crmh_v(a: &[f64],
 
     const D: usize = 10; // for statically allocating several arrays
 
-    let mut X_: [f64; 10] = [0.0; D];
     let mut lnX_: [f64; 10] = [0.0; D];
     for d in 0 .. X.len() {
-	X_[d] = X[d];
-	lnX_[d] = X_[d].ln();
+	lnX_[d] = X[d].ln();
     }
 
     // It also proves useful to tally dose-wise sums of toxicities:
-    let mut Y: [f64; 10] = [0.0; 10]; // Static allocation sufficient for 10 doses
+    let mut Y: [f64; 10] = [0.0; D];
     for i in 0 .. y.len() {
 	match X.iter().position(|&p| p==x[i]) {
 	    Some(d) => {
@@ -107,21 +62,42 @@ fn crmh_v(a: &[f64],
 	    None => {}
 	}
     }
+    // NB: Although I could eliminate Y, by computing v_tox inside the above loop,
+    //     my aim in introducing Y is partly to prepare the way for eventually
+    //     accepting it as a parameter to this function.
 
-    // To speed up the no-tox factor iteration in crmh_ (also eliminating the need
-    // for passing X_ at all!), we now precompute some details of that loop.
+    // We can calculate the toxic term irrespective of 'a' (except for its exp(a) factor)
+    let mut log_vtox = 0.0;
+    for d in 0 .. D { // TODO: Use an iterator-based expression?
+	log_vtox += Y[d]*lnX_[d];
+    }
+
+    // To speed up the no-tox factor iteration in crmh_, we now precompute
+    // some details of that loop.
     // Initially, let me treat the w==1 case, where all I need is a D-vector of
     // counts of no-tox cases.
-    let mut notox: [i32; 10] = [0; D];
-    for d in 0 .. X.len() {
-	for i in 0 .. y.len() {
-	    if y[i] == 0 && x[i] == X[d] {
-		notox[d] += 1;
+    let mut notox: [usize; 10] = [0; D];
+    let mut W: Vec<f64> = w.to_vec(); // TODO: Rationalize the sizing
+    let mut iv: i32 = 0; // just a temp var to make a check
+    let mut iw: usize = 0;
+    for d in 0 .. X.len() { // We make a single pass through unique doses tried
+	for i in 0 .. y.len() { // ..looking for patients
+	    if y[i] == 0 && x[i] == X[d] { // ..who got that dose, without toxicity.
+		notox[d] += 1; // For each such patient we increment a dose-wise counter,
+		W[iw] = w[i];  // store that patient's weight in vector W.
+		iw += 1;       // and point to the next empty spot in W.
+		iv += 1; // (temporarily track iw as an i32 for comparison)
 	    }
 	}
     }
 
-    let v = a.iter().map(|a| crmh_(a,&lnX_,X.len(),&Y,&notox,w,s,b));
+    let v = a.iter().map(|a| if a > &709.0 { 0.0 } else {
+	let log_vconst = -0.5 * (a/s).powi(2);
+	let exp_a_ = a.exp();
+	crmh_non(&exp_a_,X.len(),&lnX_,&notox,&W)
+	    * (log_vconst + exp_a_*log_vtox).exp()
+	    * a.powi(b)
+    });
     v.collect_robj()
 }
 
--- tests/testthat/test-crm.R
+++ tests/testthat/test-crm.R
@@ -18,7 +18,26 @@ test_that("Faster objective functions integrate same as 'dfcrm' originals", {
               )
   )
 
-  expect_equal(integrals$dfcrm, integrals$rusti)
+  with(integrals,
+       expect_equal(dfcrm, rusti))
+
+  ## Now again, but with weights not all 1.0:
+  w <- runif(n = length(y), min=0.8, max=1.0)
+
+  nontrivial_weights <- list(
+    dfcrm = c(integrate(dfcrm::crmh, -Inf, Inf, x, y, w, s)$value
+             ,integrate(dfcrm::crmht, -Inf, Inf, x, y, w, s)$value
+             ,integrate(dfcrm::crmht2, -Inf, Inf, x, y, w, s)$value
+              )
+  , rusti = c(integrate(crmh, -Inf, Inf, x, y, w, s)$value
+             ,integrate(crmht, -Inf, Inf, x, y, w, s)$value
+             ,integrate(crmht2, -Inf, Inf, x, y, w, s)$value
+              )
+  )
+
+  with(nontrivial_weights,
+       expect_equal(dfcrm, rusti))
+
 })
 
 test_that("crm() yields same result as dfcrm::crm, but faster", {
@@ -27,15 +46,58 @@ test_that("crm() yields same result as dfcrm::crm, but faster", {
   target <- 0.2
   level <- c(3, 4, 4, 3, 3, 4, 3, 2, 2, 2)
   y <- c(0, 0, 1, 0, 0, 1, 1, 0, 0, 0)
-  mb_old <- microbenchmark(old <- dfcrm::crm(prior, target, y, level))
-  mb_new <- microbenchmark(new <- crm(prior, target, y, level, impl="rusti"))
+  crm_old <- microbenchmark(old <- dfcrm::crm(prior, target, y, level))
+  crm_new <- microbenchmark(new <- crm(prior, target, y, level, impl="rusti"))
 
-  ###expect_equal(old$ptox, new$ptox) # TODO: Do a fuller check; consider waldo::compare()
-  expect_equal(old, new) # TODO: Do a fuller check; consider waldo::compare()
+  expect_equal(old, new)
 
   ## Expect DEFINITE improvement, with new UPPER quartile < old LOWER:
-  expect_lt(summary(mb_new, unit="ms")$uq,
-            summary(mb_old, unit="ms")$lq)
+  expect_lt(summary(crm_new, unit="ms")$uq,
+            summary(crm_old, unit="ms")$lq)
 
-  speedup_message(mb_new, mb_old)
+  speedup_message(crm_new, crm_old)
 })
+
+## test_that("titecrm() yields same result as dfcrm::crm, but faster", {
+##   ## This set-up is verbatim from the example in dfcrm::titecrm
+##   prior <- c(0.05, 0.10, 0.20, 0.35, 0.50, 0.70)
+##   target <- 0.2
+##   level <- c(3, 3, 3, 4, 4, 3, 2, 2, 2, 3)
+##   y <- c(0, 0, 1, 0, 1, 0, 0, 0, 0, 0)
+##   u <- c(178, 181, 168, 181, 24, 181, 179, 102, 42, 3)
+##   tau <- 180
+##   foo <- titecrm(prior, target, y, level, followup=u, obswin=tau)
+##   rec <- foo$mtd  # recommend a dose level for next patient
+
+##   ## An example with adaptive weight
+##   foo2 <- titecrm(prior, target, y, level, followup=u, obswin=tau, scheme="adaptive")
+##   wts <- foo2$weights
+
+##   ## The 'weights' argument makes 'followup' and 'obswin' obsolete
+##   foo3 <- titecrm(prior, target, y, level, weights=wts, followup=u, obswin=tau)
+##   \dontrun{plot(foo3, ask=T)}
+
+##   ## Patient time information via 'entry' and 'exit' arguments
+##                                         # entry time (days since study begins)
+##   entry <- c(7, 29, 49, 76, 92, 133, 241, 303, 363, 402)
+##                                         # exit time (days since study begins)
+##   exit <- c(185, 210, 217, 257, 116, 314, 420, 405, 405, 405)
+##   foo4 <- titecrm(prior, target, y, level, exit=exit, entry=entry, obswin=tau)
+##   \dontrun{plot(foo4, ask=T)}
+
+##   prior <- c(0.05, 0.10, 0.20, 0.35, 0.50, 0.70)
+##   target <- 0.2
+##   level <- c(3, 4, 4, 3, 3, 4, 3, 2, 2, 2)
+##   y <- c(0, 0, 1, 0, 0, 1, 1, 0, 0, 0)
+##   tite_old <- microbenchmark(old <- dfcrm::crm(prior, target, y, level))
+##   tite_new <- microbenchmark(new <- crm(prior, target, y, level, impl="rusti"))
+
+##   ##expect_equal(old$ptox, new$ptox) # TODO: Do a fuller check; consider waldo::compare()
+##   expect_equal(old, new) # TODO: Do a fuller check; consider waldo::compare()
+
+##   ## Expect DEFINITE improvement, with new UPPER quartile < old LOWER:
+##   expect_lt(summary(tite_new, unit="ms")$uq,
+##             summary(tite_old, unit="ms")$lq)
+
+##   speedup_message(tite_new, tite_old)
+## })
