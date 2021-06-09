411e8acf8ec5c7a4ed3cda9614b461cc2759f42c
David Norris <david@precisionmethods.guru>
2021-06-09 16:02:44 -0400

    Add seconds_since/2 and minutes_since/2

--- exec/prolog/benchmarking.pl
+++ exec/prolog/benchmarking.pl
@@ -2,7 +2,9 @@
 
 :- module(benchmarking, [
 	      version/1,
-	      time/2
+	      time/2,
+	      seconds_since/2,
+	      minutes_since/2
 	  ]).
 
 version(Version) :-
@@ -14,10 +16,18 @@ version(Version) :-
 time(Goal, Elapsed) :-
         '$cpu_now'(T0),
         (   Goal,
-            since_elapsed(T0, Elapsed)
-	;   since_elapsed(T0, Elapsed)
+            seconds_since(Elapsed, T0)
+	;   seconds_since(Elapsed, T0)
 	).
 
 since_elapsed(T0, Elapsed) :-
-        '$cpu_now'(T),
-        Elapsed is T - T0.
+    '$cpu_now'(T),
+    Elapsed is T - T0.
+
+seconds_since(Seconds, T0) :-
+    '$cpu_now'(T),
+    Seconds is T - T0.
+
+minutes_since(Minutes, T0) :-
+    seconds_since(Seconds, T0),
+    Minutes is Seconds/60.0.
