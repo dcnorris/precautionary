9e51138f376f3267245bf3a729f5c58868007547
David Norris <david@precisionmethods.guru>
2021-07-19 13:51:01 -0400

    Notes from call, plus later reflections

--- exec/prolog/monot.pl
+++ exec/prolog/monot.pl
@@ -27,6 +27,32 @@
 %?- 1/4 ~~ 3/6.
 %@    true. % if patients 5 & 6 both experience toxicities, we get 1+2=3
 
+%% To get to the heart of the matter, I need to ask for all tallies
+%% from which a given tally could be reached, then show 'we missed one'.
+
+%?- T/N ~~ 2/6, N in 0..5, indomain(N), T in 0..N.
+%@    T = 0, N = 0
+%@ ;  N = 1, clpz:(T+5#=_A), clpz:(_A in 5..6), clpz:(T in 0..1)
+%@ ;  N = 2, clpz:(T+4#=_A), clpz:(_A in 4..6), clpz:(T in 0..2)
+%@ ;  N = 3, clpz:(T+3#=_A), clpz:(_A in 3..5), clpz:(T in 0..2)
+%@ ;  N = 4, clpz:(T+2#=_A), clpz:(_A in 2..4), clpz:(T in 0..2)
+%@ ;  N = 5, clpz:(T+1#=_A), clpz:(_A in 2..3), clpz:(T in 1..2).
+
+%?- T/N ~~ 2/6, N in 0..5, indomain(N), T in 0..N, indomain(T).
+%@    T = 0, N = 0
+%@ ;  T = 0, N = 1
+%@ ;  T = 1, N = 1
+%@ ;  T = 0, N = 2
+%@ ;  T = 1, N = 2
+%@ ;  T = 2, N = 2
+%@ ;  T = 0, N = 3
+%@ ;  T = 1, N = 3
+%@ ;  T = 2, N = 3
+%@ ;  T = 0, N = 4
+%@ ;  T = 1, N = 4
+%@ ;  T = 2, N = 4
+%@ ;  T = 1, N = 5
+%@ ;  T = 2, N = 5.
 
 %% In investigating reachability, could we ever get tripped up by the
 %% non-monotonicity of clpz?
@@ -106,6 +132,21 @@
 % a #=/2, and 'put a # on it' automatically? What *legitimate* goal
 % would thereby be made impossible to write?
 
+%?- (X+1) in 0..10.
+%@ caught: error(type_error(integer,_1384249+1),unknown(_1384249+1)-1)
+
+%@    true.
+
+%?- must_be(integer, X).
+%@ caught: error(instantiation_error,must_be/2)
+
+%?- N = X + O, T/N ~~ 1/6, T #> 1, N #>= T, N #< 6, X in 0..12, O in 0..24.
+%@    N = X+O, clpz:(_A#=max(0,_B)), clpz:(_C#=max(0,_D)), clpz:(_D+6#=_E), clpz:(_B+_F#=6), clpz:(X+O#=_G), clpz:(X+O#=_E), clpz:(X+O#=_F), clpz:(T+_A#=_H), clpz:(1+_C#=_I), clpz:(T#=<X+O), clpz:(_A#>=_B), clpz:(_C#>=_D), clpz:(_I#>=T), clpz:(_G in 0..5), clpz:(_E in 0..10), clpz:(_F in 0..10), clpz:(_D in-6..4), clpz:(_B in-4..6), clpz:(_A in 0..6), clpz:(_C in 1..4), clpz:(_H in 2..11), clpz:(X in 0..5), clpz:(_I in 2..5), clpz:(T in 2..5), clpz:(O in 0..5).
+%@    N = X+O, clpz:(_A#=max(0,_B)), clpz:(_C#=max(0,_D)), clpz:(_D+6#=_E), clpz:(_B+_F#=6), clpz:(T+_A#=_G), clpz:(X+O#=_H), clpz:(X+O#=_E), clpz:(X+O#=_F), clpz:(1+_C#=_I), clpz:(T#=<X+O), clpz:(_A#>=_B), clpz:(_C#>=_D), clpz:(_I#>=T), clpz:(_H in-10..5), clpz:(_E in-10..17), clpz:(_F in-10..17), clpz:(_D in-16..11), clpz:(_B in-11..16), clpz:(_A in 0..16), clpz:(_C in 1..11), clpz:(_G in 2..28), clpz:(O in-10..5), clpz:(_I in 2..12), clpz:(T in 2..12), clpz:(X in 0..12).
+%@    N = X+O, clpz:(_A#=max(0,_B)), clpz:(_C#=max(0,_D)), clpz:(_B+_E#=6), clpz:(_D+6#=_F), clpz:(T+_A#=_G), clpz:(X+O#=_E), clpz:(X+O#=_F), clpz:(X+O#=_H), clpz:(1+_C#=_I), clpz:(T#=<X+O), clpz:(_A#>=_B), clpz:(_C#>=_D), clpz:(_I#>=T), clpz:(_A in 0..sup), clpz:(_C in 0..sup), clpz:(_I in 2..sup), clpz:(_G in 2..sup), clpz:(T in 2..sup), clpz:(_H in inf..5), clpz:(O in inf..4), clpz:(X in 1..sup).
+%@    N = X+O, clpz:(_A#=max(0,_B)), clpz:(_C#=max(0,_D)), clpz:(_B+_E#=6), clpz:(_D+6#=_F), clpz:(T+_A#=_G), clpz:(X+O#=_E), clpz:(X+O#=_F), clpz:(X+O#=_H), clpz:(1+_C#=_I), clpz:(T#=<X+O), clpz:(_A#>=_B), clpz:(_C#>=_D), clpz:(_I#>=T), clpz:(_A in 0..sup), clpz:(_C in 0..sup), clpz:(_I in 2..sup), clpz:(_G in 2..sup), clpz:(T in 2..sup), clpz:(_H in inf..5).
+
+
 
 %?- assertz(clpz:monotonic).
 %@    true.
@@ -124,3 +165,28 @@
 
 %?- #N #= #X + #O.
 %@    clpz:(#X+ #O#= #N).
+
+/*
+
+"You meant to exclude these solutions, but you didn't." -- MT
+
+"By using clpz:monotonic, you enforce that the clean representation is used."
+
+"If you have doubts about the completeness of the answer, then [you're done for.]"
+
+"If something is lost within the system... then I have to reimplement the whole system."
+
+"If I get solutions that I meant to exclude, then I can exclude them. I can write a verifier..."
+
+"But if the system doesn't REPORT solutions that it does accept..." then how could we trust it?
+
+"The key selling point is that we get everything that there is."
+
+%% TODO: Stress generalization/specialization techniques. Must demonstrate
+%%       we have a program that we can generalize readily.
+
+%% TODO: The take-away from the call and the 'factorial.pl' exploration is (I think)
+%%       that we don't want the system to invite 'magical thinking', allowing us to
+%%       freely discuss solutions with representations that WERE NEVER INTENDED.
+
+*/
