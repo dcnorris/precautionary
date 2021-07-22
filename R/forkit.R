4ceeb91ee3c119c805eb7a40f5f45f19c55a8cca
David Norris <david@precisionmethods.guru>
2021-07-22 14:26:47 -0400

    Progress toward generic 'proglapply'

--- R/forkit.R
+++ R/forkit.R
@@ -1,45 +1,78 @@
 ## Experiments with IPC for forked processes
 
-## See https://stackoverflow.com/a/27729791/3338147 by @fotNelton
-
 library(parallel)
 
-## TODO: I'd like to do something like sumSquaresTo below,
-##       except that the polling loop should be in the main
-##       process, and the main process should manage the UI
-##       progress reporting, then (when work done) receive
-##       the results of mclapply().
-##
-## This may very well be too much to ask! Perhaps I must do
-## the work of mclapply myself, using mcparallel + mccollect.
+## This design begins to look promising. But I need a clear nomenclature!
+## Let me say that a body of 'work' is parallelized into 'tasks' that are
+## 'parceled' into 'jobs', and that these jobs are 'assigned' to 'workers'.
+## We can also say the tasks are 'parceled out' to the workers.
 
-sumSqool <- function(n) {
-  f <- fifo(tempfile(), open="w+b", blocking=TRUE)
-  on.exit(close(f))
+## TODO: Should I reserve 'job' for work that is underway by a worker?
 
-  ## Here's the 'work' to be done in parallel:
-  work <- function(x) {
-    Sys.sleep(x/2)
-    # Contribute to the answer...
-    x2 <- x^2
-    writeBin(x2, f)
-    x2
+## This is represented by a 'task matrix' with workers (identified by pid)
+## assigned (as rownames) to rows that represent the parcels as sequences
+## of 'task indexes' (possibly NA) into the 'task list'.
+
+## These matrices, although 'ragged' and generally quite likely to contain
+## NA indexes (because work can't be evenly divided), do give handy means
+## to represent and compute with the parcellation.
+
+proglapply <- function(X, FUN
+                     , parcellator = NULL # use default defined in body
+                     , metric = length # progress metric on task results
+                     , handler = NULL # use body's default progress handler
+                     , ... # arguments to pass to progress reporting?
+                     , workers = parallelly::availableCores(omit = 2)
+                       ) {
+  ## Parcel out the work to the workers
+  N <- ((length(X) + workers - 1) %/% workers) * workers
+  m <- matrix(1:N, nrow = workers)
+  m[m > length(X)] <- NA # task matrix is generally 'ragged'
+
+  ## Get workers started on their jobs, with per-task progress messaging
+  jobs <- lapply(1:workers, function(w)
+    mcparallel(lapply(X[na.exclude(m[w,])], function(x) {
+      result <- FUN(x)
+      parallel:::sendMaster(metric(result), raw.asis=FALSE)
+      result
+    }), name = w))
+  rownames(m) <- sapply(jobs, function(.).$pid)
+
+  cat("Work was parcelled out as follows:\n")
+  print(m)
+
+  ## We track progress reports due per worker, to avoid `readChild`-ing
+  ## the final results, which would steal them from `mccollect`:
+  progressReportsDue <- rowSums(!is.na(m)) # count non-NA tasks for each worker
+  cat("Expecting the following progress reports:\n")
+  print(progressReportsDue)
+
+  cat("Watching progress notifications...\n")
+  while (sum(progressReportsDue) > 0) {
+    dueToReport = as.integer(names(progressReportsDue)[progressReportsDue > 0])
+    ## TODO: The following is not restricting polling to the desired values!
+    pids <- parallel:::selectChildren(dueToReport)
+    if (!is.integer(pids))
+      next
+    msg <- parallel:::readChild(pids[1])
+    if (is.raw(msg)) {
+      pid <- attr(msg,'pid')
+      if (!(pid %in% dueToReport)) {
+        cat("ERROR!\n")
+        print(dueToReport)
+      }
+      pid <- as.character(attr(msg,'pid'))
+      cat("Received message from pid", pid, "\n")
+      cat("Message says:", unserialize(msg), "\n")
+      ##cat(sprintf("Sum: %d\n", sum)) # Here's where I'd do Shiny UI update!
+      progressReportsDue[pid] <- progressReportsDue[pid] - 1
+      print(progressReportsDue)
+    } else { # this branch should never happen now
+      if (is.integer(msg))
+        stop("Received notice from exiting pid:", msg, "\n")
+    }
   }
 
-  ## Use the 'naive parallel lapply' from `?mccollect`
-  jobs <- lapply(1:n, function(x) mcparallel(work(x), name = x))
-
-  cat("Watching results come in...\n")
-  sum <- 0
-  jobsDone <- 0L
-  while (jobsDone < length(jobs) && !isIncomplete(f)) {
-    msg <- readBin(f, "double")
-    sum <- sum + as.numeric(msg)
-    cat(sprintf("Sum: %d\n", sum)) # Here's where I'd do Shiny UI update!
-    jobsDone <- jobsDone + 1
-  }
-
-  cat("Collecting results...\n")
   mccollect(jobs)
 }
 
@@ -121,71 +154,3 @@ sumComp <- function(n=10, mc.cores = 5) {
   cat("Collecting results...\n")
   mccollect(jobs)
 }
-
-
-## Add the numbers (1:n)^2 by posting them to a FIFO
-
-sumSquaresTo <- function(n) {
-  f <- fifo(tempfile(), open="w+b", blocking=TRUE)
-
-  ## Anticipating that the parent process will suspend while
-  ## waiting for the mclapply call below to return, we spawn
-  ## a child dedicated to collecting the results from those
-  ## parallel workers:
-  if (inherits(parallel:::mcfork(), "masterProcess")) { # 'spawning' idiom
-    ## Child 'collector' process
-    sum <- 0
-    answer <- sum((1:n)^2) # TODO: How can I avoid needing to know this?
-    while (sum < answer && !isIncomplete(f)) {
-      msg <- readBin(f, "double")
-      sum <- sum + as.numeric(msg)
-      cat(sprintf("Sum: %d\n", sum)) # Q: Can a child process update Shiny UI?
-      ## I rather suspect that only the main event-loop can update the
-      ## Shiny UI for me. So perhaps I need to handle this collecting
-      ## task in the main function, and delegate rather the mclapply()
-      ## invocation to a child?
-    }
-    ## Q: What is the role of !isIncomplete(f) above? Presumably,
-    ##    the while loop should exit in case isIncomplete(f); why?
-    ##    The documentation states that isIncomplete() returns
-    ##    "whether the last read attempt was blocked," which here
-    ##    would presumably be the readBin(f) from the previous
-    ##    iteration? What would it mean that this read blocked?
-    ##    Would that mean the fifo has been closed?
-    ##    Or does this possibly serve to catch exceptional conditions,
-    ##    related to the 'adjustments' to blocking described here?
-    ##
-    ## > Blocking:
-    ## >   ...
-    ## >   In blocking mode, functions using the connection do not return to
-    ## >   the R evaluator until the read/write is complete.  In non-blocking
-    ## >   mode, operations return as soon as possible, so on input they will
-    ## >   return with whatever input is available (possibly none) and for
-    ## >   output they will return whether or not the write succeeded.
-    ## >   ...
-    ## >   Even when a connection is in blocking mode, attempts are made to
-    ## >   ensure that it does not block the event loop and hence the
-    ## >   operation of GUI parts of R.  These do not always succeed, and the
-    ## >   whole R process will be blocked during a DNS lookup on Unix, for
-    ## >   example.
-    parallel:::mcexit()
-  }
-
-  ## Now the main work begins:
-  numJobs <- n
-  result <- mclapply(1:numJobs, function(x) {
-    Sys.sleep(1)
-    # Contribute to the answer...
-    x2 <- x^2
-    writeBin(x2, f)
-    x2
-  }
-  , mc.cores = 5)
-  close(f)
-  result
-}
-
-
-## TODO: Why not use non-blocking fifos, per the default behavior?
-##       Given that my message sizes are 'atomic' (just 1 integer!),
-##       don't I have a pretty good shot at making this work?
